7ac5f286093f8bcde940c972940edd96c6c4ab00
Yexiang Yan <yexiang_yan@outlook.com>
2026-03-21 18:31:22 -0400

    feat: add dash-panel support

--- opstool/vis/plotly/__init__.py
+++ opstool/vis/plotly/__init__.py
@@ -5,9 +5,11 @@ from ...utils import make_dependency_missing
 if not find_spec("plotly"):
     extra = "plotly"
     plot_model = make_dependency_missing("plotly.plot_model", "plotly", extra=extra)
+    plot_model_dash = make_dependency_missing("plotly.vis_model_dash.plot_model_dash", "plotly", extra=extra)
     plot_eigen = make_dependency_missing("plotly.plot_eigen", "plotly", extra=extra)
     plot_eigen_animation = make_dependency_missing("plotly.plot_eigen_animation", "plotly", extra=extra)
     plot_eigen_table = make_dependency_missing("plotly.plot_eigen_table", "plotly", extra=extra)
+    plot_eigen_dash = make_dependency_missing("plotly.vis_eigen_dash.plot_eigen_dash", "plotly", extra=extra)
     plot_frame_responses = make_dependency_missing("plotly.plot_frame_responses", "plotly", extra=extra)
     plot_frame_responses_animation = make_dependency_missing(
         "plotly.plot_frame_responses_animation", "plotly", extra=extra
@@ -30,8 +32,10 @@ if not find_spec("plotly"):
 else:
     from .plot_utils import reset_plot_props, set_plot_colors, set_plot_props
     from .vis_eigen import plot_eigen, plot_eigen_animation, plot_eigen_table
+    from .vis_eigen_dash import plot_eigen_dash
     from .vis_frame_resp import plot_frame_responses, plot_frame_responses_animation
     from .vis_model import plot_model
+    from .vis_model_dash import plot_model_dash
     from .vis_nodal_resp import plot_nodal_responses, plot_nodal_responses_animation
     from .vis_truss_resp import plot_truss_responses, plot_truss_responses_animation
     from .vis_unstru_resp import plot_unstruct_responses, plot_unstruct_responses_animation
@@ -41,12 +45,14 @@ reset_plot_props()
 __all__ = [
     "plot_eigen",
     "plot_eigen_animation",
+    "plot_eigen_dash",
     "plot_eigen_table",
     # # --------------------------------
     "plot_frame_responses",
     "plot_frame_responses_animation",
     # --------------------------------
     "plot_model",
+    "plot_model_dash",
     # --------------------------------
     "plot_nodal_responses",
     "plot_nodal_responses_animation",
